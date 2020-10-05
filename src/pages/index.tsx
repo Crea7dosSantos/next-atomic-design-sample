f983c8f467aa6b262acde483e53c685219a20ba5
Crea7dosSantos <cr26yusaku0720@icloud.com>
2020-10-05 14:15:11 +0900

    close #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import React from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout, { siteTitle } from '../components/layout'
 import utils from '../../styles/utils.module.scss'
 
-export default function Home() {
+export default function Home({ blogs }) {
   return (
     <Layout home>
       <Head>
@@ -11,11 +13,39 @@ export default function Home() {
       </Head>
       <section className={utils.headingMd}>
         <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <div>
+          <h2>最新の記事</h2>
+          <div>
+            {blogs.map(blog => (
+              <React.Fragment key={blog.id}>
+                <Link href="/blogs/[id]" as={`blogs/${blog.id}`}>
+                  <a>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
+                  </a>
+                </Link>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY }
+  }
+  const res = await fetch(
+    'https://next-jamstack.microcms.io/api/v1/blogs',
+    key
+  )
+  const data = await res.json()
+
+  return {
+    props: {
+      blogs: data.contents
+    }
+  }
 }
